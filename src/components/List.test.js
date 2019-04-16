30ebbe2dea3cf58a405a0a24a234bd480fd1a79e
Adam Pavlicek <adamhpavlicek@gmail.com>
2019-04-16 10:59:15 -0500

    add List.test.js

---
+++ src/components/List.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './List';
+import renderer from 'react-test-renderer';
+
+describe('List_testing', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div')
+
+        ReactDOM.render(<List/>,div)
+
+        ReactDOM.unmountComponentAtNode(div);
+    })
+
+    it('renders with no crashing', () =>{
+        const tree = renderer.create(<List key={5} header={[]} cards={[]}/>).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    })
+
+})
+
