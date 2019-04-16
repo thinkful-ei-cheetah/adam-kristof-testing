ff88c27aef666a69aa6ee19509349010f96112b4
Adam Pavlicek <adamhpavlicek@gmail.com>
2019-04-16 11:22:45 -0500

    add App.test.js

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import List from './List';
+import App from '../App';
+import STORE from '../Store'
 import renderer from 'react-test-renderer';
 
-describe('List_testing', () => {
+describe('App_testing', () => {
     it('renders without crashing', () => {
         const div = document.createElement('div')
 
-        ReactDOM.render(<List key={34} header={[]} cards={[]}/>,div);
+        ReactDOM.render(<App store={{lists:[], allCards:{}}}/>,div);
 
         ReactDOM.unmountComponentAtNode(div);
     })
 
     it('renders with no crashing', () =>{
-        const tree = renderer.create(<List key={5} header={[]} cards={[]}/>).toJSON();
+        const tree = renderer.create(<App store={{lists:[], allCards:{}}} />).toJSON();
 
         expect(tree).toMatchSnapshot();
     })
